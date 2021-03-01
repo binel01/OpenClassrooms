f07ec0cbdf19b5695ffd66a1df2d464b6a534e84
Fuzzy Parabellum <antrod.pro@hotmail.fr>
2021-03-01 16:12:39 +0100

    avant d'envoyer sur github

--- 2appScraping/Scraping.py
+++ 2appScraping/Scraping.py
@@ -3,5 +3,19 @@ from bs4 import BeautifulSoup
 
 
 webpage = requests.get('http://books.toscrape.com/catalogue/category/books/mystery_3/index.html')
+soup = BeautifulSoup(webpage.content, "html.parser")
 
-print(webpage.text)
+books_titles = soup.select("li h3 a")
+
+paragraph_printer = []
+for text in books_titles:
+    paragraph_printer.append(text.attrs["title"])
+
+"""for i in paragraph_printer:
+    print(i)"""
+
+books_prices = soup.find_all(attrs={'class':'price_color'})
+"""
+for price in books_prices:
+    print(price.string)"""
+
